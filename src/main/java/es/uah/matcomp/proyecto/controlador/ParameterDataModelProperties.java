11b07311ec2779e48c7ddcd78baf7731216b9eed
mariadelaorden <mariadelaorden2005@hotmail.com>
2024-05-20 13:49:48 +0200

    arreglos errores

--- src/main/java/es/uah/matcomp/proyecto/controlador/ParameterDataModelProperties.java
+++ src/main/java/es/uah/matcomp/proyecto/controlador/ParameterDataModelProperties.java
@@ -2,15 +2,20 @@
 package es.uah.matcomp.proyecto.controlador;
 
 import es.uah.matcomp.proyecto.modelo.individuo.PlantillaIndividuo;
+import es.uah.matcomp.proyecto.modelo.recurso.*;
 import es.uah.matcomp.proyecto.modelo.tablero.Tablero;
 import javafx.beans.property.*;
 
 public class ParameterDataModelProperties {
+
+    private static ParameterDataModelProperties instance;
+
     //Modelo de datos original
     protected Tablero originalTablero;
     protected PlantillaIndividuo individuoBasico;
     protected PlantillaIndividuo individuoNormal;
     protected PlantillaIndividuo individuoAvanzado;
+
 
     private IntegerProperty ancho = new SimpleIntegerProperty();
     private IntegerProperty largo = new SimpleIntegerProperty();
@@ -26,9 +31,25 @@ public class ParameterDataModelProperties {
     private DoubleProperty probReproduccionAvanzado = new SimpleDoubleProperty();
     private DoubleProperty probClonacionAvanzado = new SimpleDoubleProperty();
 
-    public ParameterDataModelProperties(Tablero originalTablero, PlantillaIndividuo individuoBasico,
-                                        PlantillaIndividuo individuoNormal, PlantillaIndividuo individuoAvanzado) {
+    private DoubleProperty probAparicionRecurso = new SimpleDoubleProperty();
+    private DoubleProperty probTesoro = new SimpleDoubleProperty();
+    private DoubleProperty probAgua = new SimpleDoubleProperty();
+    private DoubleProperty probComida = new SimpleDoubleProperty();
+    private DoubleProperty probMontaña = new SimpleDoubleProperty();
+    private DoubleProperty probBiblioteca = new SimpleDoubleProperty();
+    private DoubleProperty probPozo = new SimpleDoubleProperty();
+
+    private ParameterDataModelProperties(Tablero originalTablero, PlantillaIndividuo individuoBasico,
+                                         PlantillaIndividuo individuoNormal, PlantillaIndividuo individuoAvanzado) {
         setOriginal(originalTablero, individuoBasico, individuoNormal, individuoAvanzado);
+    }
+
+    public static ParameterDataModelProperties getInstance(Tablero originalTablero, PlantillaIndividuo individuoBasico,
+                                                           PlantillaIndividuo individuoNormal, PlantillaIndividuo individuoAvanzado) {
+        if (instance == null) {
+            instance = new ParameterDataModelProperties(originalTablero, individuoBasico, individuoNormal, individuoAvanzado);
+        }
+        return instance;
     }
 
     public void commit() {
@@ -43,6 +64,14 @@ public class ParameterDataModelProperties {
         individuoAvanzado.setVida(vidasAvanzado.get());
         individuoAvanzado.setProbReproduccion(probReproduccionAvanzado.get());
         individuoAvanzado.setProbClonacion(probClonacionAvanzado.get());
+
+        Recurso.setProbAparicion(probAparicionRecurso.get());
+        Tesoro.setProbAparicion(probTesoro.get());
+        Agua.setProbAparicion(probAgua.get());
+        Comida.setProbAparicion(probComida.get());
+        Biblioteca.setProbAparicion(probBiblioteca.get());
+        Montana.setProbAparicion(probMontaña.get());
+        Pozo.setProbAparicion(probPozo.get());
     }
 
     public void rollback() {
@@ -57,6 +86,14 @@ public class ParameterDataModelProperties {
         vidasAvanzado.set(individuoAvanzado.getVida());
         probReproduccionAvanzado.set(individuoAvanzado.getProbReproduccion());
         probClonacionAvanzado.set(individuoAvanzado.getProbClonacion());
+
+        probAparicionRecurso.set(Recurso.getProbAparicion());
+        probTesoro.set(Tesoro.getProbAparicion());
+        probAgua.set(Recurso.getProbAparicion());
+        probComida.set(Recurso.getProbAparicion());
+        probMontaña.set(Recurso.getProbAparicion());
+        probBiblioteca.set(Recurso.getProbAparicion());
+        probPozo.set(Recurso.getProbAparicion());
     }
 
     public Tablero getOriginalTablero() {
@@ -81,6 +118,8 @@ public class ParameterDataModelProperties {
         this.individuoBasico = individuoBasico;
         this.individuoNormal = individuoNormal;
         this.individuoAvanzado = individuoAvanzado;
+
+
         rollback(); //Se inicializan los properties.
     }
 
@@ -126,5 +165,33 @@ public class ParameterDataModelProperties {
 
     public Property<Number> probClonacionAvanzadoProperty() {
         return probClonacionAvanzado;
+    }
+
+    public Property<Number> probAparicionRecursoProperty() {
+        return probAparicionRecurso;
+    }
+
+    public Property<Number> probTesoroProperty() {
+        return probTesoro;
+    }
+
+    public Property<Number> probAguaProperty() {
+        return probAgua;
+    }
+
+    public Property<Number> probComidaProperty() {
+        return probComida;
+    }
+
+    public Property<Number> probMontañaProperty() {
+        return probMontaña;
+    }
+
+    public Property<Number> probBibliotecaProperty() {
+        return probBiblioteca;
+    }
+
+    public Property<Number> probPozoProperty() {
+        return probPozo;
     }
 }
--- src/main/java/es/uah/matcomp/proyecto/controlador/TableroController.java
+++ src/main/java/es/uah/matcomp/proyecto/controlador/TableroController.java
@@ -1,20 +1,27 @@
 
 package es.uah.matcomp.proyecto.controlador;
 
+import es.uah.matcomp.proyecto.estructurasdedatos.listas.ElementoLS;
+import es.uah.matcomp.proyecto.estructurasdedatos.listas.ListaSimple;
 import es.uah.matcomp.proyecto.modelo.individuo.Individuo;
 import es.uah.matcomp.proyecto.modelo.individuo.TipoIndividuo;
-import es.uah.matcomp.proyecto.modelo.tablero.Celda;
-import es.uah.matcomp.proyecto.modelo.tablero.Tablero;
+import es.uah.matcomp.proyecto.modelo.tablero.CeldaInfo;
+import es.uah.matcomp.proyecto.modelo.tablero.CustomLabel;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -22,13 +29,6 @@ import java.util.ResourceBundle;
 public class TableroController extends GridPane implements Initializable {
     @FXML
     private GridPane tableroGridPane;
-    private Tablero tablero;
-
-    private Stage scene;
-
-    private Stage parametersScene;
-    private ParameterDataModelProperties modeloParaGUICompartido;
-
     @FXML
     private Slider sliderVidas;
     @FXML
@@ -38,44 +38,65 @@ public class TableroController extends GridPane implements Initializable {
     @FXML
     private ChoiceBox<String> seleccionTipo;
 
+    private boolean tableroCreado = false;
+    private Stage scene;
+
+    private Stage parametersScene;
+    private ParameterDataModelProperties modeloParaGUICompartido;
+
+    private Timeline timeline;
+
+    private int generacionActual = 0;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        timeline = new Timeline(new KeyFrame(Duration.seconds(2), event -> {
+            // Aquí van las operaciones que se deben realizar cada 2 segundos
+            buclePrincipal();
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
     }
 
     public void setParametersScene(Stage parametersScene) {
         this.parametersScene = parametersScene;
     }
 
-    public void setModeloParaGUICompartido(ParameterDataModelProperties modeloParaGUICompartido) {
-        this.modeloParaGUICompartido = modeloParaGUICompartido;
-        this.tablero = modeloParaGUICompartido.getOriginalTablero();
+    public void setModeloParaGUICompartido() {
+
+        this.modeloParaGUICompartido = ParameterDataModelProperties.getInstance(null, null, null, null);
     }
 
     public void crearTablero() {
-        for (int i = 0; i < this.tablero.getLargo(); i++) {
-            for (int j = 0; j < this.tablero.getAncho(); j++) {
+        for (int i = 0; i < this.modeloParaGUICompartido.originalTablero.getLargo(); i++) {
+            for (int j = 0; j < this.modeloParaGUICompartido.originalTablero.getAncho(); j++) {
                 // Crear celdaLabel
-                Label celdaLabel = new CustomLabel(i, j, "");
-                celdaLabel.setMinSize(30, 30);
-                celdaLabel.setStyle("-fx-background-color: white; -fx-border-color: black; -fx-text-alignment: center;");
+                Label celdaLabel = new CustomLabel(this.modeloParaGUICompartido.originalTablero.getCelda(i, j), this.modeloParaGUICompartido);
 
-                // Agregar opciones
-                // celdaLabel.setOnMouseClicked(event -> mostrarOpcionesCelda(celdaLabel));
-                celdaLabel.setOnMouseClicked(event -> addIndividuo((CustomLabel) event.getSource()));
+                // Abrir menu de info si se hace click izquierdo
+                celdaLabel.setOnMouseClicked(event -> {
+                    if (event.getButton() == MouseButton.PRIMARY) {
+                        CeldaInfo celdaInfo = new CeldaInfo();
+                        CustomLabel celda = (CustomLabel) event.getSource();
+                        celdaInfo.updateCelda(celda.getCelda());
+                        Stage stage = new Stage();
+                        stage.setTitle("Información de la celda");
+                        stage.setScene(new Scene(celdaInfo, 700, 400));
+                        stage.show();
+                    }
+                });
+
 
                 // Añadir celdaLabel
                 tableroGridPane.add(celdaLabel, i, j);
             }
         }
-        tablero.resetearTablero();
+//        this.modeloParaGUICompartido.originalTablero.resetearTablero();
     }
 
-    private void addIndividuo(CustomLabel celdaLabel) {
-        System.out.println("Clicked position: " + celdaLabel.getI() + " " + celdaLabel.getJ());
-        Celda celda = tablero.getCelda(celdaLabel.getI(), celdaLabel.getJ());
-        celda.addIndividuo(new Individuo(modeloParaGUICompartido.getIndividuoBasico(), 1, TipoIndividuo.BASICO));
-        celdaLabel.setStyle("-fx-background-color: black;");
+    public void updateBoard() {
+        for (Node node: this.tableroGridPane.getChildren()) {
+            ((CustomLabel) node).updateLabel();
+        }
     }
 
     public void setStage(Stage s){
@@ -87,9 +108,11 @@ public class TableroController extends GridPane implements Initializable {
     }
 
     public void onGuardarButtonClick(ActionEvent actionEvent) {
+        this.modeloParaGUICompartido.originalTablero.imprimirTablero();
     }
 
     public void onReiniciarButtonClick(ActionEvent actionEvent) {
+
     }
 
     public void onParametrosButtonClick(ActionEvent actionEvent) {
@@ -100,70 +123,88 @@ public class TableroController extends GridPane implements Initializable {
             stage.setTitle("Parametros");
             stage.setScene(scene);
             ParametersController parametersController = fxmlLoader.getController();
-            parametersController.loadUserData(this.modeloParaGUICompartido);
+            parametersController.loadUserData();
             parametersController.setStage(stage);
             parametersController.setOpenedFromMainWindow(false);
             parametersController.setPrevStage(this.scene);
             parametersController.disableTableroTab();
+            parametersController.updateGUIwithModel();
             stage.show();
-            this.scene.close();
+//            this.scene.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void loadUserData(ParameterDataModelProperties modeloParaGUICompartido) {
-        this.tablero = modeloParaGUICompartido.getOriginalTablero();
+    public void onPlayButtonClick(ActionEvent actionEvent) {
+        timeline.play();
     }
 
-//    private void mostrarOpcionesCelda(Label celda) {
-//        ContextMenu menu = new ContextMenu();
-//        MenuItem opcion1 = new MenuItem("Añadir individuo");
-//        MenuItem opcion2 = new MenuItem("Añadir recurso");
-//
-//        opcion1.setOnAction(event -> {
-//            TextInputDialog dialog = new TextInputDialog();
-//            dialog.setTitle("Añadir Individuo");
-//            dialog.setHeaderText(null);
-//            dialog.setContentText("Identificacion individuo:");
-//
-//            Optional<String> result = dialog.showAndWait();
-//            result.ifPresent(identificacion -> {
-//                // Capturar los valores de los sliders
-//                int vidas = (int) sliderVidas.getValue();
-//                double probReproduccion = sliderProbReproduccion.getValue();
-//                double probClonacion = sliderProbClonacion.getValue();
-//                String tipoIndividuo = seleccionTipo.getValue();
-//
-//                // Crear un nuevo individuo con los valores de los sliders
-//                Individuo individuo = new Individuo(vidas, probReproduccion, probClonacion, tipoIndividuo);
-//
-//                // Agregar el nuevo individuo a la lista
-//                individuos.add(individuo);
-//
-//                // Marcar la celda como ocupada
-//                celda.setText("I");
-//            });
-//        });
-//
-//        menu.getItems().addAll(opcion1, opcion2);
-//
-//        menu.show(celda, Side.BOTTOM, 0, 0);
-//    }
+    public void onPauseButtonClick(ActionEvent actionEvent) {
+        timeline.pause();
+    }
 
-//
-//    public void initialize(URL url, ResourceBundle resourceBundle) {
-//        // Vincular los sliders a las propiedades adecuadas
-//        sliderVidas.valueProperty().addListener((observable, oldValue, newValue) -> {
-//            // Manejar el cambio de valor, si es necesario
-//        });
-//
-//        sliderProbReproduccion.valueProperty().addListener((observable, oldValue, newValue) -> {
-//            // Manejar el cambio de valor, si es necesario
-//        });
-//
-//        sliderProbClonacion.valueProperty().addListener((observable, oldValue, newValue) -> {
-//            // Manejar el cambio de valor, si es necesario
-//        });
-//    }
+    public void onStopButtonClick(ActionEvent actionEvent) {
+        timeline.stop();
+    }
+
+    private void actualizarVidaIndividuos() {
+        // Actualizar la vida de los individuos
+        for (Node node: this.tableroGridPane.getChildren()) {
+            ListaSimple individuos = ((CustomLabel) node).getCelda().getIndividuos();
+
+            if (individuos.getNumeroElementos() > 0) {
+                System.out.println("Individuos en celda antes del delete: (tamaño: " + individuos.getNumeroElementos());
+                individuos.imprimir();
+            }
+
+            for (int i = 0; i < individuos.getMaximo(); i++) {
+                ElementoLS el = individuos.getElemento(i);
+                if (el != null) {
+                    System.out.println("El indice " + i + " es: " + el.getData());
+                    Individuo ind = (Individuo) individuos.getElemento(i).getData();
+                    ind.setVida(ind.getVida() - 1);
+                    if (ind.getVida() <= 0) {
+                        individuos.del(i);
+
+                        System.out.println("Despues del delete: ");
+                        individuos.imprimir();
+                    }
+                }
+
+            }
+        }
+    }
+
+    private void buclePrincipal() {
+        // Implementar las operaciones que se deben realizar cada 2 segundos
+        // Añadir un individuo en la posicion tempContadorX
+        Individuo ind = new Individuo(this.modeloParaGUICompartido.individuoBasico, generacionActual, TipoIndividuo.BASICO);
+//        Celda celda = this.modeloParaGUICompartido.originalTablero.getCelda(tempContadorX, 0);
+//        celda.addIndividuo(ind);
+        generacionActual++;
+        System.out.println("Generación actual: " + generacionActual);
+
+        // Para cada individuo, se actualiza su tiempo de vida, y en su caso se elimina si ha muerto.
+        this.actualizarVidaIndividuos();
+
+        //  Para cada recurso, evaluará si sigue activo o debe eliminarse (por su tiempo de aparición).
+
+        //  Se ejecutará el movimiento de cada individuo (siempre obligatorio).
+
+        //  Para cada individuo evaluará las mejoras obtenidas por los distintos recursos que se
+        //encuentren en su nueva posición.
+
+        // Para cada posición, evaluará si existe reproducción o no.
+
+        // Para cada individuo, evaluará si existe clonación o no.
+
+        // Para cada posición del tablero en la que existan varios individuos, se evaluará si deben
+        //desaparecer algunos.
+
+        // Para cada posición del tablero, se evaluará si deben aparecer nuevos recursos.
+        // IMPORTANTE, FALTA NORMALIZAR LA PROBABILIDAD DE APARICIÓN DE LOS RECURSOS. (las V del enunciado)
+
+        this.updateBoard();
+    }
 }
